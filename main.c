e694b38932f00b1169fefce9feb8abff854da357
philip-egan <37160057+philip-egan@users.noreply.github.com>
2018-03-08 15:09:12 +0000

    Add files via upload
    working structures

--- main.c
+++ main.c
@@ -4,38 +4,41 @@
 #include<string.h>
 
  char board[8][8]={"x"};
- 
+ //player structure
 struct player
 {
 	char name[20];
 	char colour[10];
 	int tokens;
 };
-
+//main is used for players to assign their names and receive a random colour
 int main (void)
 {
 	struct player player1;
 	struct player player2;
 	printf("please enter player Ones Name\n");
 	fgets(player1.name,20,stdin);
-	printf("%s\n",player1.name);
+
 	printf("please enter player Twos Name\n");
 	fgets(player2.name,20,stdin);
 	
+	player1.tokens=32;
+	player2.tokens=32;
 	
-	
+	//temp is used for the if/ else
+	//this will assign player 1 +2 a random colour to start the game
 	int temp = rand()%2;
 	if(temp==0)
 	{
-		player1.colour=="1";
-		player2.colour=="0";
+		strcpy(player1.colour, "1");
+		strcpy(player2.colour, "0");
 	}
 	else{
-		player1.colour=="0";
-		player2.colour=="1";
+		strcpy(player1.colour, "0");
+		strcpy(player2.colour, "1");
 	}
-	printf("%s\n",player1.colour);
-	printf("%s is : %s\n",player2.name,player2.colour);
+	printf("player 1 :%s\n",player1.colour);
+	printf("player 2 : %s\n",player2.colour);
 }
 
 int start( )
