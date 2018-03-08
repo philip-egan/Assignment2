fab4b31860e1e2d1aaf068b818ab146bc3a6b3c5
philip-egan <37160057+philip-egan@users.noreply.github.com>
2018-03-08 15:00:23 +0000

    Add files via upload
    structs2.0

--- main.c
+++ main.c
@@ -3,11 +3,48 @@
 #include<math.h>
 #include<string.h>
 
-
+ char board[8][8]={"x"};
+ 
 struct player
 {
-	char name;
-	char colour;
+	char name[20];
+	char colour[10];
+	int tokens;
+};
+
+int main (void)
+{
+	struct player player1;
+	struct player player2;
+	printf("please enter player Ones Name\n");
+	fgets(player1.name,20,stdin);
+	printf("%s\n",player1.name);
+	printf("please enter player Twos Name\n");
+	fgets(player2.name,20,stdin);
+	
+	
+	
+	int temp = rand()%2;
+	if(temp==0)
+	{
+		player1.colour=="1";
+		player2.colour=="0";
+	}
+	else{
+		player1.colour=="0";
+		player2.colour=="1";
+	}
+	printf("%s\n",player1.colour);
+	printf("%s is : %s\n",player2.name,player2.colour);
+}
+
+int start( )
+{
 	
 }
 
+int print( char board[8][8])
+{
+	
+	
+}
