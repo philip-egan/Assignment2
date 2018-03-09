7ee6b672a7de1230a67fa542d85943bbef662401
philip-egan <37160057+philip-egan@users.noreply.github.com>
2018-03-09 11:42:58 +0000

    LibraresForFurthanEdit
    has all but the main

---
+++ libraries.h
@@ -0,0 +1,6 @@
+
+struct player(char name[]);
+
+void start();
+
+void printBoard()
---
+++ structures.c
@@ -0,0 +1,64 @@
+ //player structure
+struct player
+{
+	char name[20];
+	char colour[10];
+	int tokens;
+	int tokensLeft;
+};
+//structures for the disk 
+struct disks
+{
+	char colour[10];
+	int position[2];
+}disk[64];
+
+struct board{
+	
+	grid[8][8];
+};
+
+
+void start()
+{
+	for(int i = 0; i < 8; i++)
+	{
+		for(int j = 0; j < 9; j++)
+		{
+			if((i == 3 && j == 3) || (i == 4 && j == 4))
+			{
+				board.grid[i][j] = '1';
+			}
+			else if((i == 3 && j == 4) || (i == 4 && j == 3))
+			{
+				board.grid[i][j] = '0';
+			}
+			else
+			{
+				board.grid[i][j] = 'x';
+			}
+		}
+	}
+}
+
+void printBoard()
+{
+	int i, j;
+	
+	printf("%3c",' ');
+	for(i = 1; i <= 8; i++ )
+	{
+		printf("%3d", i);
+	}
+	printf("\n");
+	for(i = 0; i < 8; i++)
+	{
+		printf("%3d", (i+1));
+		for(j = 0; j < 8; j++)
+		{
+			printf("%3c", board.grid[i][j]);
+		}
+		printf("\n");
+	}
+	
+}
